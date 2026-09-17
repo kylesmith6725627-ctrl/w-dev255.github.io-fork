e3aba9edd0a656ce0d603f0a1feeca1cf01c3ba5
w-dev255 <devangleteam@gmail.com>
2026-09-17 19:45:21 +0200

    update

--- res/script/agent.js
+++ res/script/agent.js
@@ -1,36 +1,35 @@
-document.body.style.backgroundColor = 'black';
-
-const supported_language = ["JavaScript"];
+document.body.style.backgroundColor = '#000000';
+document.body.style.border = '3px solid green';
 
 let buffer = [];
 
 let command = [];
-let language = [];
 let output = [];
 
-let entry = document.createElement('p');
-entry.textContent = '~#';
-entry.style.color = 'green';
-entry.style.fontFamily = 'monospace';
-document.body.appendChild(entry);
-
 let commandArea = document.createElement('textarea');
-commandArea.style.border = "none";
-commandArea.style.backgroundColor = 'black';
+commandArea.className = 'input_metod';
+commandArea.id = '1438223782977';
+commandArea.style.backgroundColor = '#000000';
 commandArea.style.color = 'green';
 commandArea.style.fontFamily = 'monospace';
-commandArea.style.width = '100px';
-commandArea.style.height = '50px';
+commandArea.style.height = '5rem'
+commandArea.style.width = '10rem';
 document.body.appendChild(commandArea);
 
-commandArea.addEventListener('keydown', (event) => {
-	if (event.key === "Enter") {
-		event.preventDefault();
-		if (commandArea.value.trim().length === 0) {
-			return;
-		} else {
-			buffer.push(commandArea.value.trim());
-			commandArea.value = '';
-		}
+let button = document.createElement('button');
+button.style.type = 'submit';
+button.style.className = 'input_metod';
+button.style.id = '6695881929728';
+button.style.backgroundColor = '#000000';
+button.style.color = 'green';
+button.style.fontFamily = 'monospace';
+button.style.border = '3px solid green';
+button.style.padding = '6px';
+document.body.appendChild(button);
+
+button.addEventListener('click', () => {
+	if (commandArea.value.trim().length === 0) {
+		return;
 	}
+	buffer.push(commandArea.value.trim());
 });

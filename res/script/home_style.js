2cac7559265989ed0944a207e432fd60d346337a
w-dev255 <devangleteam@gmail.com>
2026-09-17 16:15:41 +0200

    update

--- res/script/home_style.js
+++ res/script/home_style.js
@@ -1,2 +1,12 @@
 document.body.style.backgroundColor = '#000000';
+document.body.style.border = '3px solid green';
+document.body.style.padding = '6px';
+
+let icon = document.createElement('img');
+icon.src = "res/img/w-dev255_black_icon.jpg";
+icon.className = 'icon';
+
+let search_icon = document.querySelector('.icon');
+search_icon.style.height = '50px';
+search_icon.style.width = '25px';
 
